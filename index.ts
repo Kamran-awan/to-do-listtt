909484b8d33ca3ebb85be307c24bec7d9cecde7a
Kamran-awan <kamranawan343@gmail.com>
2024-04-15 21:49:02 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos =[];
+let condition=true;
+
+while(condition)
+{
+    let addTask =await inquirer.prompt(
+        [
+            {
+                name : "todo",
+                type:"input",
+                message:"daily grocery list"
+            
+            },
+            {
+                name:"addMore",
+                type:"confirm",
+                message:"What did you buy",
+                default:"False"
+
+            }
+        ]
+    );
+
+    todos.push(addTask.todo);
+        condition =addTask.addMore
+        console.log(todos)
+    
+
+}
